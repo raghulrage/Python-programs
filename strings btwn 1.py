e76ee091dfdd7d7baa555ef38788a79bf5bc0b73
raghulrage <46341793+raghulrage@users.noreply.github.com>
2019-12-05 09:51:13 +0530

    Add files via upload

---
+++ strings btwn 1.py
@@ -0,0 +1,20 @@
+n=int(input())
+for i in range(n):
+    num=list(input())
+    l=[]
+    for i in range(len(num)):
+        if(num[i]=='1'):
+            l.append(i)
+    k=0
+    if(len(l)<2):
+        print('No string exists')
+    else:
+        for i in range(0,len(l)-1):
+            m=0
+            for j in range(len(l)-1):
+                m+=1
+                s1=l[k]
+                s2=l[m]
+                if(s1<s2):
+                    print(''.join(map(str,num[s1:s2+1])))
+            k+=1
