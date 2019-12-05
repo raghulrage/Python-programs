b0b7ba49ae002c464f18725026b21fad94bf15db
raghulrage <46341793+raghulrage@users.noreply.github.com>
2019-12-05 19:39:17 +0530

    Add files via upload

--- question paper.py
+++ question paper.py
@@ -3,25 +3,21 @@ start=int(input("Enter 1 to start quiz"))
 if(start==1):
     qns=["Author of Python","Which function is used to display a user content","Which function is used to get data from user"]
     opts=[["GVR","Steve Jobs","J G","P N"],["print()","input()","format()","eval()"],["print()","input()","format()","eval()"]]
-    answer=["GVR","print()","input()"]
+    answer=[opts[0][0],opts[1][0],opts[2][1]]
+    mark=[0,0,0]
     i=0
-    mark=0
-    while(i<3 and i>=0):
+    while(i<3):
         print("your Question Q{}\n".format(i+1))
         print("Description {}\n".format(qns[i]))       
         print("Options {}\n".format(opts[i]))
         ans=input("typer your answer")
         if(ans==answer[i]):
-            mark+=1
-            print(mark)
+            mark[i]=1
         else:
-            if(mark>0):
-                mark-=1
-                print(mark)
+            mark[i]=0
         nxt=int(input("Select your options\n1.Next\n2.Previous"))
-
         if(nxt==1):
             i+=1
-        else:
+        elif(i>0):
             i-=1
-        
+    print("YOUR score",sum(mark),'out of 3')
