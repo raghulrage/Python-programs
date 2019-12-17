d45971c65463e57abce3514736c991ac7b09d324
Raghul Thangavel <46341793+raghulrage@users.noreply.github.com>
2019-12-17 11:27:45 +0530

    Update largest combination.py

--- largest combination.py
+++ largest combination.py
@@ -19,3 +19,11 @@ for i in z:
         s=s+str(j)
     n.append(int(s))
 print(max(n))
+
+#solution
+a = input().split()
+max_num_in_list = len(max(a))+1
+l1 = [(max_num_in_list*i[:max_num_in_list:] , i ) for i in a ]
+l1.sort(reverse=True)
+for i in range(len(l1)):
+    print(l1[i][1],end="")
