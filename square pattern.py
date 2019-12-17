b39d4e2d8ee30f1ae56e1c53756aac82eedc8d98
Raghul Thangavel <46341793+raghulrage@users.noreply.github.com>
2019-12-17 12:09:55 +0530

    Update square pattern.py

--- square pattern.py
+++ square pattern.py
@@ -1,21 +1,20 @@
 n=4
-for k in range(1):
     a=0
     b=0
-    for i in range(n,0,-1):
-        for j in range(i):
-            print("*",end=' ')
-        for l in range(a):
-            print(" ",end=' ')
-        a+=1
-        print(end=' ')
-        
-        for x in range(b):
-            print(" ",end=' ')
-        b+=1
-        for y in range(i):
-            print("*",end=' ')
-        print()
+for i in range(n,0,-1):
+    for j in range(i):
+        print("*",end=' ')
+    for l in range(a):
+        print(" ",end=' ')
+    a+=1
+    print(end=' ')
+
+    for x in range(b):
+        print(" ",end=' ')
+    b+=1
+    for y in range(i):
+        print("*",end=' ')
+    print()
 a=n-1
 for i in range(n):
     for j in range(i+1):
