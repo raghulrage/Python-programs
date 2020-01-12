ced621ec8e21d25457c13c7ec602b91a9cd9a9ec
Raghul Thangavel <46341793+raghulrage@users.noreply.github.com>
2020-01-12 10:46:05 +0530

    Update str-?-btwn-no.sum-of-10.py

--- Basic-programs/str-?-btwn-no.sum-of-10.py
+++ Basic-programs/str-?-btwn-no.sum-of-10.py
@@ -3,13 +3,14 @@ def fn(a):
         if(a[i].isdigit()):
             for j in range(i+1,len(a)):
                 if((a[j].isdigit())):
-                    if(int(a[i])+int(a[j])==10):
-                        return True 
-                        break
+                    if(int(a[i])+int(a[j])==10):+ry7e
+                        if(a[i:j].count('?')==3):
+                            return True 
+                            break
     return False
 
 
-a="aa6?9"
+a="aa6???4"
 
 if(fn(a)==True):
     print('True')
@@ -17,18 +18,17 @@ else:
     print("False")
     
 '''
-Have the function QuestionsMarks(str) take the str string parameter, 
-which will contain single digit numbers, letters, and question marks, and
+Have the function QuestionsMarks(str) take the str string parameter, which will contain single digit numbers, letters, and question marks, and
 check if there are exactly 3 question marks between every pair of two numbers that add up to 10.
 If so, then your program should return the string true, otherwise it should return the string false.
 If there aren't any two numbers that add up to 10 in the string, then your program should return false as well.
 
 For example: if str is "arrb6???4xxbl5???eee5"
-then your program should return true because there are exactly 3 question marks between 6 and 4, and 3 questionmarks
-between 5 and 5 at the end of the string.
+then your program should return true because there are exactly 3 question marks between 6 and 4, and 3 question marks between 5 and 5 at the end of the string.
 Examples
 Input: "aa6?9"
 Output: false
 Input: "acc?7??sss?3rr1??????5"
 Output: true
 '''
+
