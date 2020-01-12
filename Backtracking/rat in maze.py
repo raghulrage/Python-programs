3903be42f75d5720a0a5a8c10c1bc8af420ba67d
Raghul Thangavel <46341793+raghulrage@users.noreply.github.com>
2020-01-12 20:17:36 +0530

    Update rat in maze.py

--- Backtracking/rat in maze.py
+++ Backtracking/rat in maze.py
@@ -1,4 +1,4 @@
-#LEVEL-1,2,3:
+#LEVEL-1:
 maze = [
     [0,0,0,0,0],
     [0,1,0,1,0],
@@ -37,8 +37,61 @@ if solvemaze(0,0)==True:
 else:
     print("no solution")  
 
-    
-    
+#LEVEL-3
+maze = [
+    [0,0,0,0,0],
+    [0,1,0,1,0],
+    [0,0,1,0,0],
+    [0,0,0,0,1],
+    [1,1,1,0,0]
+]
+SIZE= len(maze)
+d=0
+mindis=[]
+all=[]
+solution = [[0]*SIZE for _ in range(SIZE)]
+
+def solvemaze(l,r):
+    global d
+    if l==SIZE-1 and r==SIZE-1:
+        d+=1
+        solution[l][r]=1
+        mindis.append(d)
+        dis=0
+        for i in solution:
+            print(i)
+        print(d)
+        print('**********************')
+        all.append(solution)
+        if(d not in mindis):
+            return True
+        return False
+
+    if l<SIZE and r<SIZE and l>=0 and r>=0 and solution[l][r]==0 and maze[l][r]==0:
+        solution[l][r]=1
+        d+=1
+        if solvemaze(l+1,r):
+            return True
+        if solvemaze(l-1,r):
+            return True
+        if solvemaze(l,r+1):
+            return True
+        if solvemaze(l,r-1):
+            return True
+        solution[l][r]=0
+        d-=1
+        return False
+    return 0
+
+if solvemaze(0,0)==True:
+    pass
+else:
+    if(mindis==[]):
+        print("no solution")
+    else:
+        print(min(mindis))
+
+        
 #LEVEL-4
 maze = [
     [0,0,1,0,0],
