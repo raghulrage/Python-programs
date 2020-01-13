4c5e11df9235a8a95fcf05e883979591aed71522
Raghul Thangavel <46341793+raghulrage@users.noreply.github.com>
2020-01-13 20:38:27 +0530

    Update rat in maze.py

--- Backtracking/rat in maze.py
+++ Backtracking/rat in maze.py
@@ -91,6 +91,8 @@ else:
             print(i)
         
 #LEVEL-4
+
+import copy
 maze = [
     [0,0,1,0,0],
     [0,1,0,0,0],
@@ -98,42 +100,50 @@ maze = [
     [0,1,0,1,0],
     [0,1,1,1,0]
 ]
-N= len(maze)
-dist=[0]
-solution = [[0]*N for _ in range(N)]
+SIZE= len(maze)
+d=0
+ans=[]
+mindis=[]
+solution = [[0]*SIZE for _ in range(SIZE)]
 
-def solvemaze(l,r,dist):
-    if maze[0][0]==1 and maze[N-1][N-1]==1 and maze[0][N-1]==1 and maze[N-1][0]==1:
+def solvemaze(l,r):
+    global d
+    if maze[0][0]==1 and maze[SIZE-1][SIZE-1]==1 and maze[0][SIZE-1]==1 and maze[SIZE-1][0]==1:
         return False
-    if ((l==N-1 and r==N-1) or (l==0 and r==N-1) or(l==0 and r==0) or (l==N-1 and r==0)) and maze[l][r]!=1:
+    if ((l==SIZE-1 and r==SIZE-1) or (l==0 and r==0) or (l==0 and r==SIZE-1) or (l==SIZE-1 and r==0))and maze[l][r]!=1:
+        d+=1
         solution[l][r]=1
-        dist[0]+=1
-        return True
-    if l<N-1 and r<N-1 and l>=0 and r>=0 and solution[l][r]==0 and maze[l][r]==0:
+        mindis.append(d)
+        dis=0
+        ans.append(copy.deepcopy(solution))
+        if(d not in mindis):
+            return True
+        d-=1
+        return False
+
+    if l<SIZE and r<SIZE and l>=0 and r>=0 and solution[l][r]==0 and maze[l][r]==0:
         solution[l][r]=1
-        dist[0]+=1
-        if solvemaze(l-1,r,dist):
+        d+=1
+        if solvemaze(l+1,r):
             return True
-        if solvemaze(l,r-1,dist):
+        if solvemaze(l-1,r):
             return True
-        if solvemaze(l+1,r,dist):
+        if solvemaze(l,r+1):
             return True
-        if solvemaze(l,r+1,dist):
+        if solvemaze(l,r-1):
             return True
-        
         solution[l][r]=0
-        dist[0]-=1
+        d-=1
         return False
     return 0
 
-
-if solvemaze(2,3,dist):
-    for i in maze:
-        print(i)
-    print()
-    for i in solution:
-        print(i)
-    print()
-    print("minimum distance is :",dist[0])
+if solvemaze(2,3)==True:
+    pass
 else:
-    print("no solution")
+    if(mindis==[]):
+        print("no solution")
+    else:
+        print(min(mindis))
+        for i in ans[mindis.index(min(mindis))]:
+            print(i)
+
