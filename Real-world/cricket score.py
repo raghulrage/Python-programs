90b6bffc7ef30d2d2e2ad48a9adc4483d6f7cce9
Raghul Thangavel <46341793+raghulrage@users.noreply.github.com>
2020-01-26 13:27:18 +0530

    Update cricket score.py

--- Real-world/cricket score.py
+++ Real-world/cricket score.py
@@ -11,8 +11,8 @@ P1…..P11=Player 1 to Player 11
 #initialization
 players=[['P1',0],['P2',0],['P3',0],['P4',0],['P5',0],
          ['P6',0],['P7',0],['P8',0],['P9',0],['P10',0],['P11',0]]
-match,overs,balls,extras,count,played,i,j,total='',0,0,0,-1,1,0,1,0
-
+match,overs,balls,extras,count,played,i,j,total='',0,0,0,0,1,0,1,0
+conditions=['W','O','.','1','2','3','4','6','.','0']
 def get_input():
     global match
     match=input()
@@ -23,31 +23,32 @@ def calculate():
         if(balls==6):
             overs+=1
             balls=0
-            i,j=j,i
-        if(count==len(match)-1):
-           break
+            i,j=j,i        
+        if(count==len(match) or played==11):
+            break
+        if(match[count] in conditions):
+            if(match[count]=='W'):
+                extras+=1
+            elif(match[count]=='.'):
+                balls+=1
+            elif(match[count].isdigit()):
+                balls+=1
+                players[i][1]+=int(match[count])
+                if(int(match[count])%2!=0 or int(match[count]==1)):
+                    i,j=j,i
+            elif(match[count]=='O'):
+                i=(max(players.index(players[i]),players.index(players[j])))+1
+                played+=1
+                balls+=1
         count+=1
-        if(match[count]=='W'):
-            extras+=1
-        elif(match[count]=='.'):
-            balls+=1
-        elif(match[count].isdigit()):
-            balls+=1
-            players[i][1]+=int(match[count])
-            if(int(match[count])%2!=0 or int(match[count]==1)):
-                i,j=j,i
-        elif(match[count]=='O'):
-            i=(max(players.index(players[i]),players.index(players[j])))+1
-            played+=1
-            balls+=1
-
 def result():
     global total,overs,balls,extras,count,played,i,j
     for m,n in players:
         if(int(players.index([m,n])<=played)):
             total+=n
             print(m,'-',str(n)+'(runs)')
-    print('Strike -',players[i][0])
+    if(played<=10):
+        print('Strike -',players[i][0])
     print('Non-Strike - '+players[j][0])
     print('Total-',extras+total)
     print('Overs -',overs+(balls/10))
@@ -56,11 +57,13 @@ def result():
     print('Remaining Wickets(s) -',len(players)-played)
 
 def main():
+    
     get_input()
     calculate()
     result()
 if __name__=='__main__':
     main()
+
 
 
 
