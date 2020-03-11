496d235bd2bd73f57201033c68abfb1b80662383
Raghul Thangavel <46341793+raghulrage@users.noreply.github.com>
2020-03-11 18:25:58 +0530

    Add files via upload

---
+++ complex/longest_unique_substring.py
@@ -0,0 +1,15 @@
+from itertools import groupby
+
+def longest(x):
+         if x in long:
+             long.clear()
+             return False
+         long.add(x)
+         return True
+        
+def find_longest(string):s
+    return len(max((list(j) for i,j in groupby(string, key=longest)),key=len))    
+
+string= input()
+long = set()
+print (find_longest(string))
